40a5d1154b9b6809ba5683f35a161a0bd2eca8ed
pikselan <faif@pikselan.com>
2020-04-11 23:07:06 +0700

    create handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  top: number;
+  left: number;
+  bottom: number;
+  right: number;
+}
+
+class App extends React.Component {
+  state: State = {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
+  }
+
+  handleChange = (event: { target: { name: any; value: any; }; }) => {
+    this.setState({ [event.target.name] : event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo" style={{borderRadius:`${this.state.top}px ${this.state.right}px ${this.state.bottom}px ${this.state.left}px`}}>
+            <span className="App-text">
+              {"Border radius"}
+            </span>
+          </div>
+          <div className="App-input">
+            <input type="number" className="top" name="top" value={this.state.top} onChange={this.handleChange} />
+            <input type="number" className="right" name="right" value={this.state.right} onChange={this.handleChange} />
+            <input type="number" className="bottom" name="bottom" value={this.state.bottom} onChange={this.handleChange} />
+            <input type="number" className="left" name="left" value={this.state.left} onChange={this.handleChange} />
+          </div>
+          <div className="App-copy">
+            <h3>Result</h3>
+            <textarea className="copy-it">
+
+            </textarea>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
