f9142b1f5bd5b6d9652d52f6223a9cb1f9112125
pikselan <faif@pikselan.com>
2020-04-12 21:19:08 +0700

    create func copy to clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import copy  from 'copy-to-clipboard';
 import './App.css';
 
 interface State {
@@ -8,6 +9,7 @@ interface State {
   right: number;
   set: string;
   unit: string;
+  copied: boolean;
 }
 
 class App extends React.Component {
@@ -17,7 +19,8 @@ class App extends React.Component {
     bottom: 0,
     right: 0,
     set: "border-radius",
-    unit: "px"
+    unit: "px",
+    copied: false
   }
 
   handleChange = (event: { target: { name: any; value: any; }; }) => {
@@ -28,8 +31,12 @@ class App extends React.Component {
 
   }
 
+  copyToClipboard = () => {
+    
+  }
+
   render() {
-    let { top, right, bottom, left, unit, set } = this.state; 
+    let { top, right, bottom, left, unit, set } = this.state;
 
     let value = `${top}${unit} ${right}${unit} ${bottom}${unit} ${left}${unit}`;
     let css = `${set}: ${value};`;
@@ -41,19 +48,24 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Border Radius style</h1>
+          <h1>Border Radius style (px)</h1>
           
-          <div className="App-logo" style={{borderRadius:`${value}`}}>
+          <div className="box">
+            <div className="App-logo" style={{borderRadius:`${value}`}}></div>
             <input type="number" className="top" name="top" value={top} onChange={this.handleChange} />
             <input type="number" className="right" name="right" value={right} onChange={this.handleChange} />
             <input type="number" className="bottom" name="bottom" value={bottom} onChange={this.handleChange} />
             <input type="number" className="left" name="left" value={left} onChange={this.handleChange} />
           </div>
+
           <div className="App-copy">
             <h3>Result</h3>
             <textarea className="copy-it" name="text" value={text} onChange={this.handleText}></textarea>
             <br />
-            <button>Copy!</button>
+            <button onClick={() => {copy(text); this.setState({copied: true})}}>Copy!</button>
+            {
+              this.state.copied ? <span>Copied</span> : null
+            }
           </div>
         </header>
       </div>
